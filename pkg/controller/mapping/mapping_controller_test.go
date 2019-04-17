21a2dc0fe178c3ccd8e116658fa87100009da03e
Lukasz Piatkowski <piontec@gmail.com>
2019-04-17 12:49:03 +0200

    reconcile test (#7)

--- pkg/controller/mapping/mapping_controller_test.go
+++ pkg/controller/mapping/mapping_controller_test.go
@@ -17,75 +17,220 @@ import (
 	"testing"
 	"time"
 
-	smartnatv1alpha1 "github.com/DevFactory/smartnat/pkg/apis/smartnat/v1alpha1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	nt "github.com/DevFactory/go-tools/pkg/nettools"
+	ntmocks "github.com/DevFactory/go-tools/pkg/nettools/mocks"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/DevFactory/smartnat/pkg/apis/smartnat/v1alpha1"
+	"github.com/DevFactory/smartnat/pkg/controller/mapping/mocks"
+	"github.com/DevFactory/smartnat/pkg/controller/mapping/testhelpers"
+
 	"github.com/onsi/gomega"
 	"golang.org/x/net/context"
-	appsv1 "k8s.io/api/apps/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1core "k8s.io/api/core/v1"
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var c client.Client
-
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
-var depKey = types.NamespacedName{Name: "foo-deployment", Namespace: "default"}
+var (
+	c                client.Client
+	mappingName            = "smartnat-test"
+	mappingNamespace       = "default"
+	serviceName            = "test"
+	nodeName               = "node1"
+	servicePort      int32 = 8080
+	externalIP             = "10.10.10.10"
+	mappingKey             = types.NamespacedName{Name: mappingName, Namespace: mappingNamespace}
+	expectedRequest        = reconcile.Request{NamespacedName: mappingKey}
+	testMapping            = &v1alpha1.Mapping{
+		ObjectMeta: v1meta.ObjectMeta{
+			Name:      mappingName,
+			Namespace: mappingNamespace,
+		},
+		Spec: v1alpha1.MappingSpec{
+			Addresses: []string{
+				externalIP,
+			},
+			AllowedSources: []string{
+				"10.10.100.0/24",
+			},
+			Ports: []v1alpha1.MappingPort{
+				{
+					Port:        9090,
+					ServicePort: 8090,
+					Protocol:    "udp",
+				},
+				{
+					Port:     servicePort,
+					Protocol: "tcp",
+				},
+			},
+			ServiceName: serviceName,
+			Mode:        "service",
+		},
+	}
+	service = &v1core.Service{
+		ObjectMeta: v1meta.ObjectMeta{
+			Name:      serviceName,
+			Namespace: mappingNamespace,
+		},
+		Spec: v1core.ServiceSpec{
+			Type:            v1core.ServiceTypeClusterIP,
+			SessionAffinity: v1core.ServiceAffinityNone,
+			Ports: []v1core.ServicePort{
+				v1core.ServicePort{
+					Port:       servicePort,
+					Protocol:   v1core.ProtocolTCP,
+					TargetPort: intstr.FromInt(10000),
+				},
+			},
+		},
+	}
+	endpoints = &v1core.Endpoints{
+		ObjectMeta: v1meta.ObjectMeta{
+			Name:      serviceName,
+			Namespace: mappingNamespace,
+		},
+		Subsets: []v1core.EndpointSubset{
+			v1core.EndpointSubset{
+				Addresses: []v1core.EndpointAddress{
+					v1core.EndpointAddress{
+						IP:       "172.16.10.10",
+						NodeName: &nodeName,
+					},
+				},
+				Ports: []v1core.EndpointPort{
+					v1core.EndpointPort{
+						Port:     10000,
+						Protocol: v1core.ProtocolTCP,
+					},
+				},
+			},
+		},
+	}
+)
 
 const timeout = time.Second * 5
 
-func setupReconcilerWithMocks(t *testing.T, mgr manager.Manager) reconcile.Reconciler {
-	deps := getReconcilerDependencies(t)
-	return newReconciler(mgr, deps.config, deps.ifaceProvider, deps.ipRouteSmartNatHelper, deps.mappingSyncer,
-		deps.scrubber, deps.heartbeatChan)
+type reconcilerDeps struct {
+	ifaceProvider     nt.InterfaceProvider
+	iprSmartNatHelper *mocks.IPRouteSmartNatHelper
+	syncer            *mocks.Syncer
+	scrubber          Scrubber
+	heartbeatChan     chan string
 }
 
-func TestReconcile(t *testing.T) {
-	g := gomega.NewGomegaWithT(t)
-	instance := &smartnatv1alpha1.Mapping{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
+func getReconcilerWithMocks(t *testing.T, mgr manager.Manager) (reconcile.Reconciler, *reconcilerDeps) {
+	cfg := testhelpers.GetTestConfig(t)
 
+	ifaceProvider, _, err := ntmocks.NewMockInterfaceProvider("eth[0-9]+", true)
+	if err != nil {
+		t.Logf("Cannot create ifaceProvider object: %v", err)
+		t.Fail()
+	}
+
+	ipRouteSNHelper := &mocks.IPRouteSmartNatHelper{}
+	syncer := &mocks.Syncer{}
+	scrubber := NewScrubber(ifaceProvider, cfg)
+	heartbeatChan := make(chan string, 1)
+
+	reconciler := newReconciler(mgr, cfg, ifaceProvider, ipRouteSNHelper, syncer, scrubber, heartbeatChan)
+	return reconciler, &reconcilerDeps{
+		ifaceProvider:     ifaceProvider,
+		iprSmartNatHelper: ipRouteSNHelper,
+		syncer:            syncer,
+		scrubber:          scrubber,
+		heartbeatChan:     heartbeatChan,
+	}
+}
+
+type testInstance struct {
+	deps     *reconcilerDeps
+	stopFunc func()
+	requests chan reconcile.Request
+}
+
+func setupTestInstance(t *testing.T, g *gomega.GomegaWithT) *testInstance {
 	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
 	// channel when it is finished.
 	mgr, err := manager.New(cfg, manager.Options{})
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	c = mgr.GetClient()
-
-	reconciler := setupReconcilerWithMocks(t, mgr)
+	reconciler, reconcilerDeps := getReconcilerWithMocks(t, mgr)
 	recFn, requests := SetupTestReconcile(reconciler)
 	g.Expect(add(mgr, recFn)).NotTo(gomega.HaveOccurred())
 
+	// setup api objects
+	err = c.Create(context.TODO(), service)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	err = c.Create(context.TODO(), endpoints)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	// Start TestManager handling requests with wrapped Reconcile function
 	stopMgr, mgrStopped := StartTestManager(mgr, g)
 
-	defer func() {
+	stopFunc := func() {
 		close(stopMgr)
 		mgrStopped.Wait()
-	}()
-
-	// Create the Mapping object and expect the Reconcile and Deployment to be created
-	err = c.Create(context.TODO(), instance)
-	// The instance object may not be a valid object because it might be missing some required fields.
-	// Please modify the instance object by adding required fields and then remove the following if statement.
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create object, got an invalid object error: %v", err)
-		return
 	}
+
+	return &testInstance{
+		deps:     reconcilerDeps,
+		stopFunc: stopFunc,
+		requests: requests,
+	}
+}
+
+func TestReconcile(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	instance := setupTestInstance(t, g)
+	defer instance.stopFunc()
+
+	// Setup mock expectations
+	instance.deps.syncer.
+		On("SyncMapping", mock.AnythingOfType("*v1alpha1.Mapping"),
+			mock.AnythingOfType("*v1.Service"), mock.AnythingOfType("*v1.Endpoints")).
+		Return(true, nil)
+
+	// Create the Mapping object and expect the Reconcile call, including heartbeat channel message
+	err := c.Create(context.TODO(), testMapping)
 	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer c.Delete(context.TODO(), instance)
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+	g.Eventually(instance.deps.heartbeatChan, timeout).Should(gomega.Receive(gomega.ContainSubstring("OK")))
+	g.Eventually(instance.requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
 
-	deploy := &appsv1.Deployment{}
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+	// Fetch the Mapping from API server
+	mapping := &v1alpha1.Mapping{}
+	g.Eventually(func() error { return c.Get(context.TODO(), mappingKey, mapping) }, timeout).
 		Should(gomega.Succeed())
 
-	// Delete the Deployment and expect Reconcile to be called for Deployment deletion
-	g.Expect(c.Delete(context.TODO(), deploy)).NotTo(gomega.HaveOccurred())
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
+	// validate mapping
+	validateMappingFromAPIServer(g, mapping)
+
+	// Delete the Mapping and expect Reconcile to be called for deletion
+	g.Expect(c.Delete(context.TODO(), mapping)).NotTo(gomega.HaveOccurred())
+	g.Eventually(instance.requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+	g.Eventually(func() error { return c.Get(context.TODO(), mappingKey, mapping) }, timeout).
 		Should(gomega.Succeed())
 
-	// Manually delete Deployment since GC isn't enabled in the test control plane
-	g.Expect(c.Delete(context.TODO(), deploy)).To(gomega.Succeed())
+	// check mock calls
+	instance.deps.iprSmartNatHelper.AssertExpectations(t)
+	instance.deps.syncer.AssertExpectations(t)
+}
 
+func validateMappingFromAPIServer(g *gomega.GomegaWithT, m *v1alpha1.Mapping) {
+	g.Expect(m.Name).To(gomega.Equal(mappingName))
+	g.Expect(m.Namespace).To(gomega.Equal(mappingNamespace))
+	g.Expect(m.ObjectMeta.CreationTimestamp).NotTo(gomega.BeZero())
+	g.Expect(m.ObjectMeta.Finalizers).To(gomega.HaveLen(1))
+	g.Expect(m.Status).ToNot(gomega.BeNil())
+	g.Expect(m.Status.Invalid).To(gomega.Equal("no"))
+	g.Expect(m.Status.ConfiguredAddresses).To(gomega.HaveLen(1))
+	_, ok := m.Status.ConfiguredAddresses[externalIP]
+	g.Expect(ok).To(gomega.BeTrue())
 }
